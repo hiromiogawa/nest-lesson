2c77ffd2bdb85d63dd277d0a68b2dd8d23877f49
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-23 21:03:09 +0900

    add: users  EOM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CatsController } from './cats.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
